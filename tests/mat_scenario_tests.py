9bb202aaeb9bc5398bba81f75e3f5607d66c1d18
Fraser Savage <fraser_sav@yahoo.co.uk>
2017-02-20 13:20:12 +0000

    Formatting changes for PEP-8

--- tests/mat_scenario_tests.py
+++ tests/mat_scenario_tests.py
@@ -1,11 +1,14 @@
-from nose.tools import *
+import nose.tools
 import mat_scenario
+
 
 def setup():
     print("SETUP!")
 
+
 def teardown():
     print("TEAR DOWN!")
+
 
 def test_basic():
     print("I RAN!")
