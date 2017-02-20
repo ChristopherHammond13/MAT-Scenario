a143739e5cd36c0b15c7e5051f9a1b0994b6eab9
Fraser Savage <fraser_sav@yahoo.co.uk>
2017-02-20 13:08:42 +0000

    Initial folder structure

---
+++ setup.py
@@ -0,0 +1,16 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'move and tag problem',
+    'author': 'graphorn',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['mat_scenario'],
+    'scripts': [],
+    'name': 'mat_scenario'
+}
+
+setup(**config)
