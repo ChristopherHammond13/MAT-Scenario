c6d9e2b84d7ced4b02691c2b8f3fac217107f556
Christopher Hammond <christopherhammond13@gmail.com>
2017-02-20 16:15:40 +0000

    Add pyvisgraph and file structure

---
+++ mat_scenario/visibility/graph.py
@@ -0,0 +1,1 @@
+import pyvisgraph as vg
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ config = {
     'description': 'move and tag problem',
     'author': 'graphorn',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose', 'pyvisgraph'],
     'packages': ['mat_scenario'],
     'scripts': [],
     'name': 'mat_scenario'
