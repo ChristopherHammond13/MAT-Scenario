5ecd07b160df5fd62c03f17d055ea9f84c87fe2c
Christopher Hammond <christopherhammond13@gmail.com>
2017-02-20 21:07:46 +0000

    Add graph constructor

--- mat_scenario/visibility/graph.py
+++ mat_scenario/visibility/graph.py
@@ -1,1 +1,17 @@
 import pyvisgraph as vg
+from multiprocessing import cpu_count
+from os.path import exists as path_exists
+
+class Map:
+
+    def __init__(self, index, points, obstacles):
+        self.index = index
+        self.points = points
+        obstacle_points = []
+        for o in obstacles:
+            ob = []
+            for p in o:
+                ob.append(vg.Point(p[0], p[1]))
+            obstacle_points.append(ob)
+        g = vg.VisGraph()
+        g.build(obstacle_points, workers=cpu_count())
